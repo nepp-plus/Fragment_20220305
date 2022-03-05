3eb577caf5321fdc43b9cbeb8c2084fdfdcbf5f5
nepp.plus <nepp.plus@gmail.com>
2022-03-05 12:49:12 +0900

    프래그먼트의 동작 관련 코드 함수 수정

--- app/src/main/java/com/neppplus/fragment_20220305/fragments/FirstFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20220305/fragments/FirstFragment.kt
@@ -1,11 +1,13 @@
 package com.neppplus.fragment_20220305.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20220305.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -18,5 +20,19 @@ class FirstFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
+
+//    프래그먼트에서의 동작 관련 코드
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnLogInFragment.setOnClickListener {
+
+            Log.d("프래그먼트", "첫번째 프래그먼트에서 로그 찍기기")
+
+
+
+    }
+
 
 }
