e0b5ac9d2c3e980edd844c6b5ae2d2b0aee2a0b7
nepp.plus <nepp.plus@gmail.com>
2022-03-05 12:44:45 +0900

    프래그먼트 클래스가, fragment_first xml을 끌어와서 사용하도록 작업

--- app/src/main/java/com/neppplus/fragment_20220305/fragments/FirstFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20220305/fragments/FirstFragment.kt
@@ -1,6 +1,22 @@
 package com.neppplus.fragment_20220305.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.neppplus.fragment_20220305.R
 
 class FirstFragment : Fragment() {
+
+//    fragment_first xml파일을, 모양으로 삼자.
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
 }
